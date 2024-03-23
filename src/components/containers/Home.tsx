42db340344da2f54cd96e06c3a6dd1a12504589d
Ayush Jain <ayushjainrksh@gmail.com>
2024-03-23 18:15:02 +0530

    Add scss and design tokens

--- src/components/containers/Home.tsx
+++ src/components/containers/Home.tsx
@@ -1,11 +1,13 @@
 import { Link, useLocation } from 'react-router-dom';
+import logo from '../../assets/images/logo.svg';
+import './styles/home.scss';
 
 const Home = (): JSX.Element => {
   const location = useLocation();
 
   return (
-    <div>
-      Home
+    <div className='home'>
+      <img src={logo} alt="logo" />
       <Link to="/login" state={{ previousLocation: location }}>Login</Link>
     </div>
   );
--- src/components/library/Modal.tsx
+++ src/components/library/Modal.tsx
@@ -1,4 +1,4 @@
-import './styles/modal.css';
+import './styles/modal.scss';
 
 interface IModalProps {
   children: JSX.Element
